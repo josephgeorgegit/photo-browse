db967d5bafffb7bb7066ea2ca4338e15759e4581
josephgeorge.git <josephgeorge.git@gmail.com>
2021-09-05 18:49:36 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,144 @@
+<template>
+<div id="app">
+  <div style="position: absolute; z-index: 99999999;">
+    <button class="paste-button" v-if="!open_paste" @click="paste_funk">Paste</button>
+    <Paste class="paste-field" v-else @links="returnLinks"/>
+  </div>
+  <div id="images">
+    <div 
+    @click="scroll" 
+    :style="{
+      transform: `translateX(-${transition_properties[0]}%)`, 
+      transition: `${transition_properties[1]}s`
+      }" 
+    style="display: flex; width: 200vw;"
+    >
+      <div v-for="image in two_images.slice(0, 2)" :key="image" class="container">
+        <img  :src="image" alt="">
+      </div>
+    </div>
+  </div>
+
+</div>
+</template>
+
+<style>
+body{
+  background-color: #020202;
+}
+#images{
+  display: flex;
+  width: 100vw !important;
+  height: 100vh;
+  display: fixed;
+  top: 0;
+  left: 0;
+  overflow-y: hidden;
+}
+.paste-field{
+  position: fixed; 
+  right: 20px; 
+  top: 20px;
+}
+.paste-button{
+  position: fixed;
+  top: 20px;
+  right: 20px;
+}
+.container{
+  height: 100vh !important;
+  width: 100vw !important;
+  display: flex;
+  justify-content: center;
+  object-fit: cover;
+}
+img{
+  object-fit: cover;
+  z-index: 0;
+  /* border-radius: 5px; */
+}
+</style>
+
+<script>
+// import lounge from '@/assets/lounge.jpg'
+// import loungeview from '@/assets/loungeview.jpg'
+// import living from '@/assets/living.jpg'
+// import outdoor from '@/assets/outdoor.jpg'
+
+// this is an aplication that lets you post anonymously a picture and you write the story of that picture.
+// everyone has a story tell, a photo says a million words. in a time where all we want is connection,
+// lets get people to tell their stories and lets get to know people anonymously. It feels like sometimes
+// we just want to tell a story but have no one to tell it to.
+
+// Most of all, this application is meant to be telling the story with the image.
+// what i wrote there is the story of why it looks like this. im giving the design a reason to exist
+
+
+// on load check how many images there are(n) and make the view width that 100 * n
+// algorithm to scale the image to optimal view size, based on height or width
+
+import Paste from '@/components/Paste.vue'
+export default {
+  name: 'App',
+  components: {
+    Paste
+  },
+  data(){
+    return{
+      position: 0,
+      images: [],
+      queue: [0, 1, 2],
+      active: false,
+      open_paste: false
+    }
+  },
+  // taking the array of images and returning 2 at a time. 
+  // on click swip the first two left/
+  // one this animation finishes load [1] into [0] and [2] from the entire array into [1]
+  computed: {
+    two_images(){
+      return [this.images[this.queue[0]], this.images[this.queue[1]], this.images[this.queue[2]]]
+    },
+    transition_properties(){
+      if(this.active){
+        return [50, 0.5]
+      }
+      return [0, 0]
+    },
+  },
+  methods: {
+    paste_funk(){
+      console.log('chehoo')
+      this.open_paste = !this.open_paste
+    },
+    returnLinks(e){
+      this.open_paste = false
+      this.images = e
+    },
+    scroll(){
+      this.active = true
+
+      setTimeout(() => {
+        if(this.two_images[1] == this.images[this.images.length-1]){
+          this.queue = [this.images.length-1, 0, 1]
+          this.active = false
+          return
+        }
+  
+        if(this.two_images[0] == this.images[this.images.length-1]){
+          this.queue = [0, 1, 2]
+          this.active = false
+          return
+        }
+  
+        this.queue = this.queue.map((index) => {return index + 1})
+        this.active = false
+        return
+      }, 500)
+
+    },
+  }
+}
+</script>
+
+
---
+++ src/components/Paste.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="paste">
+      <p style="display: flex; justify-content: space-between;">Paste in links<button @click="returnLinks">Done</button></p>
+      <textarea v-model="links" name="" id="" cols="30" rows="10"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Paste',
+    data(){
+        return{
+            links: ''
+        }
+    },
+    methods:{
+        returnLinks(){
+            this.$emit('links', this.links.split(','))
+        }
+    }
+}
+</script>
+
+<style scoped>
+.paste{
+    color: white;
+
+}
+</style>
